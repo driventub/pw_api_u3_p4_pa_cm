bfd0468f22e7d9e3c0a3c44a31ded5d638d3dc1d
paulo <aldazpaulo@gmail.com>
2023-07-22 10:42:06 -0500

    componente 3 nivel 2

--- src/main/java/uce/edu/unidad3/pw_u3_p4_pa_cm/controller/EstudianteControllerRestFul.java
+++ src/main/java/uce/edu/unidad3/pw_u3_p4_pa_cm/controller/EstudianteControllerRestFul.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import uce.edu.unidad3.pw_u3_p4_pa_cm.model.Estudiante;
@@ -27,23 +28,13 @@ public class EstudianteControllerRestFul {
     @Autowired
     private IEstudianteService estudianteService;
 
-    @GetMapping(path = "/{cedula}")
-    
-    public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String cedula){
-        
-        return ResponseEntity.status(227)
-        .body(this.estudianteService.consultarPorCedula(cedula));
+    @GetMapping(path = "/{cedula}",produces ="application/xml")
+    @ResponseStatus(HttpStatus.OK)
+    public Estudiante consultarPorCedula(@PathVariable String cedula){  
+        return this.estudianteService.consultarPorCedula(cedula);
     }
 
-    @GetMapping(path = "/status/{cedula}")
-    
-    public ResponseEntity<Estudiante> consultarPorCedulaStatus(@PathVariable String cedula){
-        
-        return ResponseEntity.status(HttpStatus.OK)
-        .body(this.estudianteService.consultarPorCedula(cedula));
-    }
-
-    @PostMapping
+    @PostMapping(consumes = "application/xml")
     public void ingresarEstudiante(@RequestBody Estudiante estu){
         this.estudianteService.insertarEstudianteService(estu);
     }
@@ -51,57 +42,37 @@ public class EstudianteControllerRestFul {
     @GetMapping
     public ResponseEntity<List<Estudiante>> buscarTodos(){
         List<Estudiante> lista = this.estudianteService.buscarTodos();
-
         HttpHeaders cabeceras = new HttpHeaders();
         cabeceras.add("detalleMensaje", "Ciudadanos consultados exitosamente");
         cabeceras.add("valorAPI", "No aplicable");
 
         return new ResponseEntity<List<Estudiante>>(lista, cabeceras, 227) ;
     }
-
-    // @PostMapping
-    // public List<Estudiante> buscarTodos(){
-        
-    //     return this.estudianteService.buscarTodos();
-    // }
     
     @PutMapping(path = "/{id}")
     public void actualizarEstudiante(@RequestBody Estudiante estu, @PathVariable Integer id){
-        // Integer id = 1;
         estu.setId(id);
-        
-
         this.estudianteService.actualizarEstudianteService(estu);
     }
 
     @PatchMapping(path = "/{cedula}")
     public void actualizarParcialEstudiante(@RequestBody Estudiante estu, @PathVariable String cedula ){
         Estudiante estu1 = this.estudianteService.consultarPorCedula(cedula);
-
         if(estu.getNombre()==null){
-
         }
-
         estu1.setCedula(estu.getCedula());
         this.estudianteService.actualizarEstudianteService(estu1);
     }
-
-    // @DeleteMapping(path = "/{id}")
-    // public void borrarId(@PathVariable Integer id){
-    //     // Integer idd = 2;
-    //     this.estudianteService.borrar(id);
-    // }
 
     @DeleteMapping(path = "/{cedula}")
     public void borrarCedula(@PathVariable String cedula){
         this.estudianteService.eliminarEstudianteService(cedula);
     }
 
-    // @GetMapping(path = "/{provincia}")
-    // public List<Estudiante> buscarTodos(@RequestParam String provincia){
-    //     // buscarTodos?provincia = pichincha
-    //     return this.estudianteService.buscarTodos(provincia);
-    // }
-    
-    
+    @GetMapping(path = "/{cedula}",produces = MediaType.APPLICATION_XML_VALUE, consumes = "application/xml")
+    public Estudiante ingresarEstudiantePersonalizado(@PathVariable String cedula , @RequestBody Estudiante estu){
+        estu.setCedula(cedula);
+        this.estudianteService.insertarEstudianteService(estu);
+        return this.estudianteService.consultarPorCedula(estu.getCedula());
+    }
 }
